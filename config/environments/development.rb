a1eadd8d6588a2a68317662594317deddb575f17
xi4oh4o <xi4oh4o@gmail.com>
2016-02-01 22:23:31 +0800

    fix(environments/development.rb): stmp信息读取环境变量名称错误修复

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => ENV["mailer_address"],
-    :port => ENV["mailer_address"],
+    :port => ENV["mailer_port"],
     :domain => ENV["mailer_domain"],
     :authentication => :login,
     :user_name => ENV["mailer_username"],
