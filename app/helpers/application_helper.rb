c6a4e7fe764faa3eaa0d0cddda97861d68e58b7e
xi4oh4o <xi4oh4o@gmail.com>
2016-02-03 16:06:49 +0800

    feat(application_helper): 添加登录表单field错误判断方法

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,16 @@ module ApplicationHelper
     resource.errors[field].empty? ? "" : "error"
   end
 
+  def valid_login_field(resource, field)
+    resource[field].empty? ? "" : "error"
+  end
+
   def flash_class_level(level)
     case level
     when "success" then "ui success message"
     when "error" then "ui error message"
-    when "notice" then "ui blue message"
-    when "alert" then "ui yellow message"
+    when "notice" then "ui success message"
+    when "alert" then "ui error message"
     end
   end
 end
