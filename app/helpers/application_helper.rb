5ffc16377d1a8a15c3e0d800dd37b893f2aead13
xi4oh4o <xi4oh4o@gmail.com>
2016-02-03 15:44:42 +0800

    feat(helpers): 优化layouts application flash提示优化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
   def valid_field(resource, field)
     resource.errors[field].empty? ? "" : "error"
   end
+
+  def flash_class_level(level)
+    case level
+    when "success" then "ui success message"
+    when "error" then "ui error message"
+    when "notice" then "ui blue message"
+    when "alert" then "ui yellow message"
+    end
+  end
 end
