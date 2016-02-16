97585637dee5fe6cc98d353ccdde0f1081be29a1
xi4oh4o <xi4oh4o@gmail.com>
2016-02-17 02:04:16 +0800

    feat(TunnelsController): 添加通道保存方法

--- app/controllers/dashboard/tunnels_controller.rb
+++ app/controllers/dashboard/tunnels_controller.rb
@@ -1,3 +1,34 @@
 class Dashboard::TunnelsController < ApplicationController
   layout "dashboard"
+
+  def index
+    @entrances = Entrance.all.collect {|entrance| [entrance.name, entrance.id]}
+    if Tunnel.last.nil?
+      @entrance_port = 30000
+    else
+      @entrance_port = Tunnel.last.entrance_port += 1
+    end
+
+    @tunnel = Tunnel.new
+  end
+
+  def create
+    @tunnel = Tunnel.new(tunnel_params)
+    if @tunnel.save
+      redirect_to dashboard_tunnels_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def tunnel_params
+      params.require(:tunnel).permit(:name,
+                    :user_id,
+                    :entrance_id,
+                    :entrance_port,
+                    :target_address,
+                    :destination_port
+                  )
+    end
 end
