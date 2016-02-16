0aadac198659a330ec59c258796d2436d8491822
xi4oh4o <xi4oh4o@gmail.com>
2016-02-16 21:21:26 +0800

    feat(migrate/20160216131850_create_tunnels): 创建通道表

---
+++ db/migrate/20160216131850_create_tunnels.rb
@@ -0,0 +1,17 @@
+class CreateTunnels < ActiveRecord::Migration
+  def change
+    create_table :tunnels do |t|
+      t.integer :user_id
+      t.integer :entrance_id
+      t.integer :entrance_port
+      t.string :target_address
+      t.integer :destination_port
+      t.integer :status
+
+      t.timestamps null: false
+    end
+    add_index :tunnels, :user_id
+    add_index :tunnels, :entrance_id
+    add_index :tunnels, :entrance_port
+  end
+end
