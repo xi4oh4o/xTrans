6097fc4cebdc72999269af545da1e9f81065e7ee
xi4oh4o <xi4oh4o@gmail.com>
2016-02-18 22:32:07 +0800

    feat(jobs/build_tunnels_job): 编写build_tunnels队列用于建立转发

---
+++ app/jobs/build_tunnels_job.rb
@@ -0,0 +1,37 @@
+class BuildTunnelsJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(args)
+    @entrance = Entrance.find(args[:entrance_id])
+    build_dnat(@entrance.ip,
+               @entrance.port,
+               args[:entrance_port],
+               args[:target_address],
+               args[:destination_port])
+  end
+
+  private
+    def build_dnat(server_ip,
+                   server_port,
+                   dport,
+                   to_ip,
+                   to_ip_port)
+
+      require 'sshkit'
+      require 'sshkit/DSL'
+      begin
+        on "root@#{server_ip}:#{server_port}" do |host|
+          execute ("iptables -t nat -A PREROUTING -p tcp -i eth0 \
+--dport #{dport} -j DNAT --to #{to_ip}:#{to_ip_port}")
+          execute "iptables -t nat -A POSTROUTING -d #{to_ip} -j MASQUERADE"
+        end
+        tunnel = Tunnel.find_by(
+                                entrance_port:    dport,
+                                target_address:   to_ip,
+                                destination_port: to_ip_port)
+        tunnel.update(status: 1)
+      rescue
+        logger.fatal "Fail with #{server_ip}:#{server_port} and dport:#{dport}"
+      end
+    end
+end
---
+++ test/jobs/build_tunnels_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BuildTunnelsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
