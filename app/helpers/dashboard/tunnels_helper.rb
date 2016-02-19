cb76bf7e03b838b6c41a22658306275d6e18771b
xi4oh4o <xi4oh4o@gmail.com>
2016-02-19 21:09:07 +0800

    feat(tunnels_helper): 添加根据入口服务器ID分配端口方法

--- app/helpers/dashboard/tunnels_helper.rb
+++ app/helpers/dashboard/tunnels_helper.rb
@@ -1,2 +1,12 @@
 module Dashboard::TunnelsHelper
+
+  def distr_port_by_entrance_id(id)
+    tunnel = Tunnel.where(entrance_id: id).last
+    if tunnel.nil?
+      '35000'
+    else
+      tunnel.entrance_port += 1
+    end
+  end
+
 end
