33f38d1aa103fb0326a3ab02e6baa1b2515ebec7
xi4oh4o <xi4oh4o@gmail.com>
2016-02-19 22:23:06 +0800

    feat(jobs/destroy_tunnels_job): 添加销毁通道队列

---
+++ app/jobs/destroy_tunnels_job.rb
@@ -0,0 +1,34 @@
+class DestroyTunnelsJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(args)
+    @tunnel = Tunnel.find(args)
+    @entrance = Entrance.find(@tunnel.entrance_id)
+    destroy_dnat(@entrance.ip,
+                 @entrance.port,
+                 @tunnel.entrance_port,
+                 @tunnel.target_address,
+                 @tunnel.destination_port
+                )
+  end
+
+  private
+    def destroy_dnat(server_ip,
+                     server_port,
+                     dport,
+                     to_ip,
+                     to_ip_port)
+
+      require 'sshkit'
+      require 'sshkit/DSL'
+      begin
+        on "root@#{server_ip}:#{server_port}" do |host|
+          execute ("iptables -t nat -D PREROUTING -p tcp -i eth0 \
+--dport #{dport} -j DNAT --to #{to_ip}:#{to_ip_port}")
+          execute "iptables -t nat -D POSTROUTING -d #{to_ip} -j MASQUERADE"
+        end
+      rescue
+        logger.fatal "Fail with #{server_ip}:#{server_port} and dport:#{dport}"
+      end
+    end
+end
