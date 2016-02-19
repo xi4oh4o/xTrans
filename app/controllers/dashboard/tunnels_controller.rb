b8582b91aae63d827bdabd8bda6a054bf5c8ef32
xi4oh4o <xi4oh4o@gmail.com>
2016-02-19 22:45:35 +0800

    feat(tunnels_controller): 添加验证登录权限

--- app/controllers/dashboard/tunnels_controller.rb
+++ app/controllers/dashboard/tunnels_controller.rb
@@ -1,5 +1,8 @@
 class Dashboard::TunnelsController < ApplicationController
+  before_action :logged_in_user, only: [:index, :create, :destroy]
+
   include Dashboard::TunnelsHelper
+
   layout "dashboard"
 
   def index
@@ -41,5 +44,13 @@ class Dashboard::TunnelsController < ApplicationController
                     :target_address,
                     :destination_port
                   )
+    end
+
+    # 确保用户已登录
+    def logged_in_user
+      unless user_signed_in?
+        flash[:danger] = "请先登录"
+        redirect_to new_user_session_url
+      end
     end
 end
