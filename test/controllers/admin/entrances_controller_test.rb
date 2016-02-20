9bb01b79e233d0445e1b08973bc3d9255b858873
xi4oh4o <xi4oh4o@gmail.com>
2016-02-20 21:38:14 +0800

    chore(rails generate): 生成入口通道控制器相关

---
+++ test/controllers/admin/entrances_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::EntrancesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/admin/tunnels_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::TunnelsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
