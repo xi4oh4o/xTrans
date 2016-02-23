606e5466426f204002404fe8c42c4246ade43404
xi4oh4o <xi4oh4o@gmail.com>
2016-02-23 21:06:39 +0800

    fix(tunnels_controller): 修复健壮参数赋值

--- app/controllers/dashboard/tunnels_controller.rb
+++ app/controllers/dashboard/tunnels_controller.rb
@@ -16,9 +16,9 @@ class Dashboard::TunnelsController < ApplicationController
 
   def create
     entrance_port = distr_port_by_entrance_id(tunnel_params[:entrance_id])
-    tunnel_params.store("entrance_port", entrance_port)
+    @tunnel_params.store("entrance_port", entrance_port)
 
-    @tunnel = Tunnel.new(tunnel_params)
+    @tunnel = Tunnel.new(@tunnel_params)
 
     if @tunnel.save
       BuildTunnelsJob.perform_later @tunnel
@@ -39,7 +39,7 @@ class Dashboard::TunnelsController < ApplicationController
 
   private
     def tunnel_params
-      params.require(:tunnel).permit(:name,
+      @tunnel_params = params.require(:tunnel).permit(:name,
                     :user_id,
                     :entrance_id,
                     :entrance_port,
