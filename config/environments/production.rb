8c28e7fb96e0f8389b963a29011cdabddcc3c75c
xi4oh4o <xi4oh4o@gmail.com>
2016-02-23 14:17:16 +0800

    feat(production.rb): add routes.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.routes.default_url_options[:host] = 'xtrans.me'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
