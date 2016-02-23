54d4827dfeaba1c7fa95056866defdb42212dc1b
xi4oh4o <xi4oh4o@gmail.com>
2016-02-23 17:17:45 +0800

    feat(EntrancesController): 添加入口添加方法

--- app/controllers/admin/entrances_controller.rb
+++ app/controllers/admin/entrances_controller.rb
@@ -8,4 +8,26 @@ class Admin::EntrancesController < ApplicationController
 
     @entrance = Entrance.new
   end
+
+  def create
+    @entrance = Entrance.new(entrance_params)
+
+    if @entrance.save
+      redirect_to admin_entrances_url
+    else
+      flash[:error] = "表单存在拼写错误"
+      redirect_to admin_entrances_url
+    end
+  end
+
+  private
+    def entrance_params
+      params.require(:entrance).permit(:name,
+                    :name,
+                    :domain,
+                    :ip,
+                    :port,
+                    :status
+                  )
+    end
 end
